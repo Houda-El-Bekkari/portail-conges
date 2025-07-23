65fa244252c27807f0b7869272439817b8972970
moad-cher <moadchergui13@gmail.com>
2025-07-23 14:10:17 +0000

    deleteRequest instead of cancelRequest + initiating the calendar component

--- force-app/main/default/lwc/calendar/calendar.js
+++ force-app/main/default/lwc/calendar/calendar.js
@@ -1,3 +1,13 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire, track } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
+import createRequest from '@salesforce/apex/Leave_Request_Controller.createRequest';
+import updateRequest from '@salesforce/apex/Leave_Request_Controller.updateRequest';
+import getRequests from '@salesforce/apex/Leave_Request_Controller.getRequests';
+import deleteRequest from '@salesforce/apex/Leave_Request_Controller.deleteRequest';
 
-export default class Calendar extends LightningElement {}
+
+export default class Calendar extends LightningElement {
+    
+}
