5c562e1cea883f7eabdb01e410dc419e5e0fc88d
moad-cher <moadchergui13@gmail.com>
2025-07-27 22:01:37 +0000

    plz work

--- force-app/main/default/lwc/fullCalendar/fullCalendar.js
+++ force-app/main/default/lwc/fullCalendar/fullCalendar.js
@@ -7,7 +7,6 @@ import { getPicklistValues, getObjectInfo } from 'lightning/uiObjectInfoApi';
 
 import createRequest from '@salesforce/apex/Leave_Request_Controller.createRequest';
 import updateRequest from '@salesforce/apex/Leave_Request_Controller.updateRequest';
-import getRequests from '@salesforce/apex/Leave_Request_Controller.getRequests';
 import deleteRequest from '@salesforce/apex/Leave_Request_Controller.deleteRequest';
 import getMyRequests from '@salesforce/apex/Leave_Request_Controller.getMyRequests';
 
